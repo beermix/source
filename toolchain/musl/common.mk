5ca915ac5fd3e1f2b922e63bffc6e58f3afc1d96
AV <27rurk@gmail.com>
2017-03-21 23:48:36 +0300

    autocommit on change

--- toolchain/musl/common.mk
+++ toolchain/musl/common.mk
@@ -35,7 +35,7 @@ MUSL_CONFIGURE:= \
 		--host=$(GNU_HOST_NAME) \
 		--target=$(REAL_GNU_TARGET_NAME) \
 		--disable-gcc-wrapper \
-		--disable-debug
+		--enable-debug
 
 define Host/Prepare
 	$(call Host/Prepare/Default)
