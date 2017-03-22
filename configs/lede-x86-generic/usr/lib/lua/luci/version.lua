ca3fd47586250e1fc2a65b93eba99dd06dd7ae04
AV <27rurk@gmail.com>
2017-03-22 08:15:48 +0300

    autocommit on change

---
+++ configs/lede-x86-generic/usr/lib/lua/luci/version.lua
@@ -0,0 +1,20 @@
+local pcall, dofile, _G = pcall, dofile, _G
+
+module "luci.version"
+
+if pcall(dofile, "/etc/openwrt_release") and _G.DISTRIB_DESCRIPTION then
+	distname    = ""
+	distversion = _G.DISTRIB_DESCRIPTION
+	if _G.DISTRIB_REVISION then
+		distrevision = _G.DISTRIB_REVISION
+		if not distversion:find(distrevision,1,true) then
+			distversion = distversion .. " " .. distrevision
+		end
+	end
+else
+	distname    = "OpenWrt"
+	distversion = "Development Snapshot"
+end
+
+luciname    = "LuCI Master"
+luciversion = "git-17.080.69173-773734e"
