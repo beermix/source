c028243cced882e76d4cdaf12e30036cad457e4f
AV <27rurk@gmail.com>
2017-09-23 22:50:38 +0300

    autocommit on change

--- include/image-commands.mk
+++ include/image-commands.mk
@@ -114,6 +114,10 @@ define Build/install-dtb
 	)
 endef
 
+define Build/install-zImage
+    $(CP) $(KDIR)/zImage \
+      $(BIN_DIR)/$(IMG_PREFIX)-$(PROFILE_SANITIZED)-zImage
+endef
 define Build/fit
 	$(TOPDIR)/scripts/mkits.sh \
 		-D $(DEVICE_NAME) -o $@.its -k $@ \
