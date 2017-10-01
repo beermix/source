11a6a7e853e548d947300d8af0dd36fb4bfeee8b
AV <27rurk@gmail.com>
2017-10-01 21:06:03 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -117,11 +117,6 @@ GCC_CONFIGURE:= \
 		--disable-multilib \
 		--disable-libmpx \
 		--disable-nls \
-		--with-gnu-as \
-		--with-gnu-ld \
-		--enable-plugin \
-		--enable-lto \
-		--enable-gold \
 		$(GRAPHITE_CONFIGURE) \
 		--with-host-libstdcxx=-lstdc++ \
 		$(SOFT_FLOAT_CONFIG_OPTION) \
