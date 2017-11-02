984af3294962b303046b11cfec30b4d75d7bf290
AV <27rurk@gmail.com>
2017-11-02 04:46:31 +0300

    autocommit on change

--- include/hardening.mk
+++ include/hardening.mk
@@ -32,7 +32,7 @@ ifdef CONFIG_PKG_FORTIFY_SOURCE_1
 endif
 ifdef CONFIG_PKG_FORTIFY_SOURCE_2
   ifeq ($(strip $(PKG_FORTIFY_SOURCE)),1)
-    TARGET_CFLAGS += -D_FORTIFY_SOURCE=2
+    TARGET_CPPFLAGS += -D_FORTIFY_SOURCE=2
   endif
 endif
 ifdef CONFIG_PKG_RELRO_PARTIAL
@@ -43,8 +43,8 @@ ifdef CONFIG_PKG_RELRO_PARTIAL
 endif
 ifdef CONFIG_PKG_RELRO_FULL
   ifeq ($(strip $(PKG_RELRO)),1)
-    TARGET_CFLAGS += -Wl,-z,now -Wl,-z,relro
-    TARGET_LDFLAGS += -znow -zrelro
+    TARGET_CFLAGS += -znow -zrelro
+    TARGET_LDFLAGS += -Wl,-O1,--sort-common,-z,relro,-z,now
   endif
 endif
 
