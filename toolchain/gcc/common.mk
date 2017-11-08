52e173571241c5317777d16c7ad19375cb3e99ce
AV <27rurk@gmail.com>
2017-11-08 13:03:29 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -113,8 +113,6 @@ GCC_CONFIGURE:= \
 		--disable-libmudflap \
 		--disable-multilib \
 		--disable-libmpx \
-		--disable-vtable-verify \
-		--disable-libitm \
 		--with-tune=generic \
 		--disable-nls \
 		$(GRAPHITE_CONFIGURE) \
