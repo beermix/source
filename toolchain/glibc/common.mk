7d952cff6934b824c2947aff6fdf8236d751ce81
AV <27rurk@gmail.com>
2017-11-12 01:16:06 +0300

    autocommit on change

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -62,10 +62,13 @@ GLIBC_CONFIGURE:= \
 		--enable-kernel=3.2.0 \
 		--disable-debug \
 		--enable-obsolete-rpc \
+		--enable-obsolete-nsl \
+		--enable-lock-elision \
 		--$(if $(CONFIG_SOFT_FLOAT),without,with)-fp
 
 export libc_cv_ssp=no
 export libc_cv_ssp_strong=no
+libc_cv_c_cleanup=yes
 export ac_cv_header_cpuid_h=yes
 export HOST_CFLAGS := $(HOST_CFLAGS) -idirafter $(CURDIR)/$(PATH_PREFIX)/include
 
