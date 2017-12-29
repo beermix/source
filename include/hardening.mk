f5a41d0cfd8c3199c23dff8754de9c4ea389b066
AV <27rurk@gmail.com>
2017-12-29 04:37:15 +0300

    autocommit on change

--- include/hardening.mk
+++ include/hardening.mk
@@ -43,7 +43,6 @@ ifdef CONFIG_PKG_RELRO_PARTIAL
 endif
 ifdef CONFIG_PKG_RELRO_FULL
   ifeq ($(strip $(PKG_RELRO)),1)
-    TARGET_LDFLAGS += -Wl,-O1,-z,relro,-z,now
+    TARGET_LDFLAGS += -Wl,-z,now -Wl,-z,relro
   endif
 endif
-
