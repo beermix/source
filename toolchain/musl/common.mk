0bb84e8a8d3e689adb63f871a7e009cc096bba1f
AV <27rurk@gmail.com>
2018-01-11 21:07:33 +0300

    autocommit on change

--- toolchain/musl/common.mk
+++ toolchain/musl/common.mk
@@ -9,12 +9,12 @@ include $(INCLUDE_DIR)/target.mk
 
 PKG_NAME:=musl
 PKG_VERSION:=1.1.18
-PKG_RELEASE=2
+PKG_RELEASE=1
 
 PKG_SOURCE_PROTO:=git
 PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_VERSION)
-PKG_SOURCE_VERSION:=d5029bb88a4ff91b007a19c7d9efb790aab63246
-#PKG_MIRROR_HASH:=a3d857c23c94aa96a4ad5f442aaf236e5a189a717273c4e4faf425988d98cd32
+PKG_SOURCE_VERSION:=72656157f54c47277b01ec85a6ba7c4084fea6c8
+PKG_MIRROR_HASH:=a3d857c23c94aa96a4ad5f442aaf236e5a189a717273c4e4faf425988d98cd32
 PKG_SOURCE_URL:=git://git.musl-libc.org/musl
 PKG_SOURCE:=$(PKG_NAME)-$(PKG_VERSION)-$(PKG_SOURCE_VERSION).tar.xz
 
@@ -26,21 +26,16 @@ HOST_BUILD_DIR:=$(BUILD_DIR_TOOLCHAIN)/$(PKG_NAME)-$(PKG_VERSION)
 include $(INCLUDE_DIR)/toolchain-build.mk
 include $(INCLUDE_DIR)/hardening.mk
 
-TARGET_CFLAGS := $(filter-out -O%,$(TARGET_CFLAGS))
-
 MUSL_CONFIGURE:= \
 	$(TARGET_CONFIGURE_OPTS) \
-	CCFLAGS="$(filter-out -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64,$(TARGET_CFLAGS)) $(MUSL_EXTRA_CFLAGS)" \
-	CPPFLAGS="$(filter-out -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64,$(TARGET_CPPFLAGS))" \
+	CFLAGS="$(TARGET_CFLAGS)" \
 	CROSS_COMPILE="$(TARGET_CROSS)" \
 	$(HOST_BUILD_DIR)/configure \
 		--prefix=/ \
 		--host=$(GNU_HOST_NAME) \
 		--target=$(REAL_GNU_TARGET_NAME) \
 		--disable-gcc-wrapper \
-		--enable-optimize=yes \
-		--enable-static \
-		--enable-shared
+		--enable-debug
 
 define Host/Prepare
 	$(call Host/Prepare/Default)
