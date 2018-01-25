641031f05d2fa302e349039f74e425fd5c4b16ff
AV <27rurk@gmail.com>
2018-01-25 13:05:47 +0300

    autocommit on change

--- include/hardening.mk
+++ include/hardening.mk
@@ -17,7 +17,7 @@ ifdef CONFIG_PKG_CHECK_FORMAT_SECURITY
 endif
 ifdef CONFIG_PKG_CC_STACKPROTECTOR_REGULAR
   ifeq ($(strip $(PKG_SSP)),1)
-    TARGET_CFLAGS += -fstack-protector
+    TARGET_CFLAGS += -fstack-protector --param=ssp-buffer-size=32 -Wl,--copy-dt-needed-entries
   endif
 endif
 ifdef CONFIG_PKG_CC_STACKPROTECTOR_STRONG
@@ -32,7 +32,7 @@ ifdef CONFIG_PKG_FORTIFY_SOURCE_1
 endif
 ifdef CONFIG_PKG_FORTIFY_SOURCE_2
   ifeq ($(strip $(PKG_FORTIFY_SOURCE)),1)
-    TARGET_CFLAGS += -D_FORTIFY_SOURCE=2
+    TARGET_CFLAGS += -Wp,-D_FORTIFY_SOURCE=2
   endif
 endif
 ifdef CONFIG_PKG_RELRO_PARTIAL
@@ -43,7 +43,7 @@ ifdef CONFIG_PKG_RELRO_PARTIAL
 endif
 ifdef CONFIG_PKG_RELRO_FULL
   ifeq ($(strip $(PKG_RELRO)),1)
-    TARGET_CFLAGS += -Wl,-z,now -Wl,-z,relro
+    TARGET_CFLAGS += -Wl,-z,now -Wl,-z,relro -Wl,-sort-common
     TARGET_LDFLAGS += -znow -zrelro
   endif
 endif
