9a9d232c4bd8a404913e45f85b15d3c1f67c87c3
AV <27rurk@gmail.com>
2018-02-02 10:16:25 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -110,6 +110,7 @@ GCC_CONFIGURE:= \
 		--with-gnu-ld \
 		--enable-target-optspace \
 		--disable-libgomp \
+		--disable-libmudflap \
 		--disable-multilib \
 		--disable-libmpx \
 		--disable-nls \
