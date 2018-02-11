95184de750a3688109b2a0dbfb259cad7c683999
AV <27rurk@gmail.com>
2018-02-11 19:39:13 +0300

    autocommit on change

--- package/firmware/linux-firmware/x86.mk
+++
@@ -1,9 +0,0 @@
-Package/amd64-microcode = $(call Package/firmware-default,AMD64 CPU microcode)
-define Package/amd64-microcode/install
-	$(INSTALL_DIR) $(1)/lib/firmware/amd-ucode
-	$(INSTALL_DATA) $(PKG_BUILD_DIR)/amd-ucode/microcode_amd.bin $(1)/lib/firmware/amd-ucode
-	$(INSTALL_DATA) $(PKG_BUILD_DIR)/amd-ucode/microcode_amd_fam15h.bin $(1)/lib/firmware/amd-ucode
-	$(INSTALL_DATA) $(PKG_BUILD_DIR)/amd-ucode/microcode_amd_fam16h.bin $(1)/lib/firmware/amd-ucode
-endef
-$(eval $(call BuildPackage,amd64-microcode))
-
