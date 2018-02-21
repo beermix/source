789f9e7cea6f271bc0123fe1eef79b811a56577a
AV <27rurk@gmail.com>
2018-02-21 10:57:36 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -113,6 +113,9 @@ GCC_CONFIGURE:= \
 		--host=$(GNU_HOST_NAME) \
 		--target=$(REAL_GNU_TARGET_NAME) \
 		--with-gnu-ld \
+		--enable-plugin \
+		--enable-lto \
+		--enable-ld=default \
 		--enable-target-optspace \
 		--disable-libgomp \
 		--disable-libmudflap \
