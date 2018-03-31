f690f85038e697b8c06937df863fd7182f6b115c
AV <27rurk@gmail.com>
2018-03-31 07:39:58 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -123,8 +123,8 @@ GCC_CONFIGURE:= \
 		--disable-multilib \
 		--disable-libmpx \
 		--disable-nls \
-		--disable-vtable-verify \
-		--disable-libunwind-exceptions \
+		--enable-checking=release \
+		--with-default-libstdcxx-abi=gcc4-compatible \
 		$(GRAPHITE_CONFIGURE) \
 		--with-host-libstdcxx=-lstdc++ \
 		$(SOFT_FLOAT_CONFIG_OPTION) \
