91cf4d9beb65a9cd3ddccfb821d7498b4a0e4512
AV <27rurk@gmail.com>
2018-04-12 06:10:54 +0300

    autocommit on change

--- include/hardening.mk
+++ include/hardening.mk
@@ -29,7 +29,7 @@ ifdef CONFIG_PKG_CC_STACKPROTECTOR_REGULAR
 endif
 ifdef CONFIG_PKG_CC_STACKPROTECTOR_STRONG
   ifeq ($(strip $(PKG_SSP)),1)
-    TARGET_CFLAGS += -fstack-protector-strong --param=ssp-buffer-size=4
+    TARGET_CFLAGS += -fstack-protector-strong --param=ssp-buffer-size=4 -fno-semantic-interposition -ftree-vectorize  -ftree-loop-vectorize
   endif
 endif
 ifdef CONFIG_PKG_FORTIFY_SOURCE_1
