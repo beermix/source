30f538065afba150dbd29b4671ba06bf36e0b78f
AV <27rurk@gmail.com>
2018-06-10 05:52:00 +0300

    autocommit on change

--- include/package-flags.mk
+++ include/package-flags.mk
@@ -66,7 +66,7 @@ ifdef CONFIG_PKG_RELRO_PARTIAL
 endif
 ifdef CONFIG_PKG_RELRO_FULL
   ifeq ($(strip $(PKG_RELRO)),1)
-    TARGET_CFLAGS += -Wl,-z,now -Wl,-z,relro -Wl,--hash-style=gnu
+    TARGET_CFLAGS += -Wl,-z,now -Wl,-z,relro
     TARGET_LDFLAGS += -znow -zrelro
   endif
 endif
