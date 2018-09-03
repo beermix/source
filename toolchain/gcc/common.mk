564baa8a9f153095f6ed98eda4613e4ece94706d
AV <27rurk@gmail.com>
2018-09-03 12:03:13 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -40,6 +40,15 @@ ifeq ($(PKG_VERSION),8.2.0)
   PKG_HASH:=196c3c04ba2613f893283977e6011b2345d1cd1af9abeac58e916b1aab3e0080
 endif
 
+ifeq ($(PKG_VERSION),8.2.1)
+  PKG_VERSION:=8.2.1
+  PKG_REV:=8.2.1-20180831
+  PKG_SOURCE_URL:=https://sources.archlinux.org/other/gcc
+  PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
+  GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
+  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
+endif
+
 ifneq ($(CONFIG_GCC_VERSION_7_1_ARC),)
     PKG_VERSION:=7.1.1
     PKG_SOURCE_URL:=https://github.com/foss-for-synopsys-dwc-arc-processors/gcc/archive/$(GCC_VERSION)
