1e23965567576dd81713ab1796bbd13bef692140
AV <27rurk@gmail.com>
2018-11-15 21:42:38 +0300

    autocommit on change

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -59,6 +59,7 @@ GLIBC_CONFIGURE:= \
 		BASH_SHELL=/bin/bash \
 		--disable-profile \
 		--enable-kernel=4.14 \
+		--enable-stack-protector=strong \
 		--without-gd \
 		--without-cvs \
 		--enable-add-ons \
