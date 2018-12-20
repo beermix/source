8eb07f897bf261d0ec339f1113809418bcddc4ca
AV <27rurk@gmail.com>
2018-12-20 14:12:24 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -40,30 +40,30 @@ ifeq ($(PKG_VERSION),8.2.0)
   PKG_HASH:=196c3c04ba2613f893283977e6011b2345d1cd1af9abeac58e916b1aab3e0080
 endif
 
-#ifeq ($(PKG_VERSION),8.2.1)
-#  PKG_VERSION:=8.2.1
-#  PKG_REV:=8.2.1-20180831
-#  PKG_HASH:=725ec907fd7463568ec0c097802824b978a679523a2e3374bdc2e3d265cd2b6c
-#  PKG_SOURCE_URL:=https://sources.archlinux.org/other/gcc
-#  PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
-#  GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
-#  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
-#endif
-
 ifeq ($(PKG_VERSION),8.2.1)
   PKG_VERSION:=8.2.1
-  PKG_REV:=8-20181214
-  PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
+  PKG_REV:=8.2.1-20181127
+  PKG_HASH:=725ec907fd7463568ec0c097802824b978a679523a2e3374bdc2e3d265cd2b6c
+  PKG_SOURCE_URL:=https://sources.archlinux.org/other/gcc
   PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
   GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
   HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
 endif
 
+#ifeq ($(PKG_VERSION),8.2.1)
+#  PKG_VERSION:=8.2.1
+#  PKG_REV:=8-20181214
+#  PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
+#  PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
+#  GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
+#  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
+#endif
+
 ifneq ($(CONFIG_GCC_VERSION_7_1_ARC),)
     PKG_VERSION:=7.1.1
     PKG_SOURCE_URL:=https://github.com/foss-for-synopsys-dwc-arc-processors/gcc/archive/$(GCC_VERSION)
     PKG_SOURCE:=$(PKG_NAME)-$(GCC_VERSION).tar.gz
-    PKG_HASH:=90596af8b9c26a434cec0a3b3d37d0c7c755ab6a65496af6ca32529fab5a6cfe
+    PKG_HASH:=
     PKG_REV:=2017.09-release
     GCC_DIR:=gcc-arc-$(PKG_REV)
     HOST_BUILD_DIR = $(BUILD_DIR_HOST)/$(PKG_NAME)-$(GCC_VERSION)
