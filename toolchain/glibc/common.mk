604ca644e836912a347c19bb625b54d6009f5e64
AV <27rurk@gmail.com>
2019-01-25 22:35:20 +0300

    autocommit on change

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -59,6 +59,7 @@ GLIBC_CONFIGURE:= \
 		BASH_SHELL=/bin/sh \
 		--disable-profile \
 		--enable-kernel=4.14 \
+		--enable-stack-protector=yes \
 		--without-gd \
 		--without-cvs \
 		--enable-add-ons \
