661f85541a98f6b2e09466f9dad13946fead83c7
AV <27rurk@gmail.com>
2019-02-19 17:08:58 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -59,9 +59,9 @@ ifeq ($(PKG_VERSION),8.2.1)
   HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
 endif
 
-ifeq ($(PKG_VERSION),9.0.0)
-  PKG_VERSION:=9.0.0
-  PKG_REV:=9-20190120
+ifeq ($(PKG_VERSION),9.0.1)
+  PKG_VERSION:=9.0.1
+  PKG_REV:=9-20190217
   PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
   PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
   GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
