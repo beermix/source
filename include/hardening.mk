c815e7803b3bdca09b4c0ac86b0c01fe44053162
AV <27rurk@gmail.com>
2019-08-09 23:34:42 +0300

    autocommit on change

--- include/hardening.mk
+++ include/hardening.mk
@@ -24,7 +24,7 @@ ifdef CONFIG_PKG_ASLR_PIE
 endif
 ifdef CONFIG_PKG_CC_STACKPROTECTOR_REGULAR
   ifeq ($(strip $(PKG_SSP)),1)
-    TARGET_CFLAGS += -fstack-protector --param=ssp-buffer-size=4
+    TARGET_CFLAGS += -fstack-protector
   endif
 endif
 ifdef CONFIG_PKG_CC_STACKPROTECTOR_STRONG
