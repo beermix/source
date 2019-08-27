ef8bdb20d5ae8252aaf7d233152c29609da530e8
AV <27rurk@gmail.com>
2019-08-27 18:56:39 +0300

    autocommit on change

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -57,7 +57,7 @@ GLIBC_CONFIGURE:= \
 		--with-binutils=$(TOOLCHAIN_DIR)/bin \
 		BASH_SHELL=/bin/sh \
 		--disable-profile \
-		--enable-kernel=4.19 \
+		--enable-kernel=4.14 \
 		--without-gd \
 		--without-cvs \
 		--enable-add-ons \
