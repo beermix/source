b19e8bc65493efb876128fd4d08981e321430b34
AV <27rurk@gmail.com>
2019-08-27 22:37:53 +0300

    autocommit on change

--- scripts/dl_github_archive.py
+++ scripts/dl_github_archive.py
@@ -345,7 +345,6 @@ class DownloadGitHubTarball(object):
         version_is_sha1sum = len(self.version) == 40
         if not version_is_sha1sum:
             apis.insert(0, apis.pop())
-        reasons = ''
         for api in apis:
             url = api['url']
             attr_path = api['attr_path']
@@ -358,9 +357,9 @@ class DownloadGitHubTarball(object):
                 self.commit_ts = ct
                 self.commit_ts_cache.set(url, ct)
                 return
-            except Exception as e:
-                reasons += '\n' + ("  {}: {}".format(url, e))
-        raise self._error('Cannot fetch commit ts:{}'.format(reasons))
+            except Exception:
+                pass
+        raise self._error('Cannot fetch commit ts: {}'.format(url))
 
     def _init_commit_ts_remote_get(self, url, attrpath):
         resp = self._make_request(url)
