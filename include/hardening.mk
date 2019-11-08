48c59f9f009cc4060b922aee9bd98b3f01be9a26
AV <27rurk@gmail.com>
2019-11-09 02:06:02 +0300

    autocommit on change

--- include/hardening.mk
+++ include/hardening.mk
@@ -50,7 +50,7 @@ ifdef CONFIG_PKG_RELRO_PARTIAL
 endif
 ifdef CONFIG_PKG_RELRO_FULL
   ifeq ($(strip $(PKG_RELRO)),1)
-    TARGET_CFLAGS += -Wl,-z,now -Wl,-z,relro
+    TARGET_CFLAGS += -Wl,-z,now -Wl,-z,relro -Wl,--hash-style=gnu -Wl,--no-copy-dt-needed-entries -Wl,--as-needed
     TARGET_LDFLAGS += -znow -zrelro
   endif
 endif
