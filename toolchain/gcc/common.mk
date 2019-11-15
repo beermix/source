7e27e25be5dcdc7ff37f4c0e1e592d6229a43a26
AV <27rurk@gmail.com>
2019-11-15 04:34:09 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -66,7 +66,7 @@ endif
 
 ifeq ($(PKG_VERSION),10.0.0)
   PKG_VERSION:=10.0.0
-  PKG_REV:=10-20191103
+  PKG_REV:=10-20191110
   PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
   PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
   GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
