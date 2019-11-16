bead42012a42a1f5b28dd9ceb6c04f84a8ec8951
AV <27rurk@gmail.com>
2019-11-16 22:18:11 +0300

    autocommit on change

---
+++ x86/64/target.mk
@@ -0,0 +1,7 @@
+ARCH:=x86_64
+BOARDNAME:=x86_64
+DEFAULT_PACKAGES += kmod-button-hotplug kmod-r8169
+
+define Target/Description
+        Build images for 64 bit systems including virtualized guests.
+endef
