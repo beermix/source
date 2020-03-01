6c5d3e2c87e8e765a9aa1c8632f8e3e1f78344ba
AV <27rurk@gmail.com>
2020-03-01 08:18:35 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -127,8 +127,8 @@ endif
 GCC_CONFIGURE:= \
 	SHELL="$(BASH)" \
 	$(if $(shell gcc --version 2>&1 | grep -E "Apple.(LLVM|clang)"), \
-		CFLAGS="-g0 -O2 -fbracket-depth=512 -pipe" \
-		CXXFLAGS="-g0 -O2 -fbracket-depth=512 -pipe" \
+		CFLAGS="-O2 -fbracket-depth=512 -pipe" \
+		CXXFLAGS="-O2 -fbracket-depth=512 -pipe" \
 	) \
 	$(HOST_SOURCE_DIR)/configure \
 		--with-bugurl=$(BUGURL) \
