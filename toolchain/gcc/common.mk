6e741e9f2be0a09499992baf2ad111a45d6b732f
AV <27rurk@gmail.com>
2020-03-10 14:49:15 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -161,6 +161,7 @@ GCC_CONFIGURE:= \
 		--disable-libstdcxx-debug \
 		--disable-libstdcxx-verbose \
 		--disable-libunwind-exceptions \
+		--disable-libstdcxx-pch \
 		--with-linker-hash-style=gnu \
 		$(GRAPHITE_CONFIGURE) \
 		--with-host-libstdcxx=-lstdc++ \
