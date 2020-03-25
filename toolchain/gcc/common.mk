ea6f3d89f8b4ead152485e5f039cbb228df906a2
AV <27rurk@gmail.com>
2020-03-25 18:03:52 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -119,8 +119,7 @@ SEP:=,
 TARGET_LANGUAGES:="c,c++$(if $(CONFIG_INSTALL_GFORTRAN),$(SEP)fortran)$(if $(CONFIG_INSTALL_GCCGO),$(SEP)go)"
 
 TAR_OPTIONS += \
-	--exclude-from='$(CURDIR)/../exclude-testsuite' --exclude=gcc/ada/*.ad* \
-	--exclude=libjava
+	--exclude=gcc/ada/*.ad* --exclude=libjava
 
 export libgcc_cv_fixed_point=no
 ifdef CONFIG_INSTALL_GCCGO
