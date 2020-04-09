251fe8faabbe3e50995f69a0cbbd03e9741d3a7b
AV <27rurk@gmail.com>
2020-04-09 15:33:21 +0300

    autocommit on change

--- target/linux/x86/generic/target.mk
+++ target/linux/x86/generic/target.mk
@@ -1,7 +1,7 @@
 BOARDNAME:=Generic
 CPU_TYPE :=pentium4
 FEATURES += pci pcie usb
-DEFAULT_PACKAGES += kmod-button-hotplug
+DEFAULT_PACKAGES += 
 
 define Target/Description
 	Build firmware images for modern x86 based boards with CPUs
