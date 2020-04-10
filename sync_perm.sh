ea56a629bcf9261a34ef4d3e87dda6936d3cd7ce
AV <27rurk@gmail.com>
2020-04-10 15:40:56 +0300

    autocommit on change

--- sync_perm.sh
+++ sync_perm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for FILE  in /home/user/ledex-git/*
 do
-    chown --reference="$FILE" /home/user/ledex/"$(basename "$FILE")"
-    chmod --reference="$FILE" /home/user/ledex/"$(basename "$FILE")"
+    chown --reference="$FILE" -R /home/user/ledex/"$(basename "$FILE")"
+    chmod --reference="$FILE" -R /home/user/ledex/"$(basename "$FILE")"
 done
