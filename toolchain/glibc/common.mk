8c343824212b1e50442fbfb1dc11a58870c7ed6f
AV <27rurk@gmail.com>
2020-04-14 13:21:40 -0500

    autocommit on change

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -58,13 +58,15 @@ GLIBC_CONFIGURE:= \
 	use_ldconfig=no \
 	$(HOST_BUILD_DIR)/$(GLIBC_PATH)configure \
 		BASH_SHELL=/bin/sh \
+		--prefix= \
 		--build=$(GNU_HOST_NAME) \
 		--host=$(REAL_GNU_TARGET_NAME) \
 		--with-headers=$(TOOLCHAIN_DIR)/include \
 		--disable-profile \
-		--without-gd \
-		--without-cvs \
 		--disable-werror \
+		--without-gd \
+		--without-selinux \
+		--without-cvs \
 		--enable-kernel=5.4 \
 		--enable-stack-protector=strong \
 		--disable-debug \
@@ -73,7 +75,7 @@ GLIBC_CONFIGURE:= \
 
 # export libc_cv_ssp=no
 # export libc_cv_ssp_strong=no
-export ac_cv_header_cpuid_h=yes
+# export ac_cv_header_cpuid_h=yes
 export HOST_CFLAGS := $(HOST_CFLAGS) -idirafter $(CURDIR)/$(PATH_PREFIX)/include
 
 define Host/SetToolchainInfo
