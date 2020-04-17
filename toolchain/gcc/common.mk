5a77ad5f5d75160ee093785026808be44ff3c884
AV <27rurk@gmail.com>
2020-04-17 17:52:17 -0500

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -138,6 +138,7 @@ GCC_CONFIGURE:= \
 		--with-tune=generic \
 		--enable-checking=release \
 		--with-linker-hash-style=gnu \
+		--enable-offload-targets \
 		$(GRAPHITE_CONFIGURE) \
 		--with-host-libstdcxx=-lstdc++ \
 		$(SOFT_FLOAT_CONFIG_OPTION) \
