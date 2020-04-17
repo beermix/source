4cebc3ea17ff2ac33fd840356f7e8c426f874abb
AV <27rurk@gmail.com>
2020-04-17 17:24:19 -0500

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -60,11 +60,18 @@ ifeq ($(PKG_VERSION),9.3.1)
 endif
 
 ifeq ($(PKG_VERSION),10.0.1)
-  PKG_VERSION:=10.0.1
-  PKG_REV:=10-20200412
-  PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
-  PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
-  GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
+#  PKG_VERSION:=10.0.1
+#  PKG_REV:=10-20200412
+#  PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
+#  PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
+#  GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
+#  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
+  PKG_REV:=a28edad
+  PKG_SOURCE_PROTO:=git
+  PKG_SOURCE_URL:=https://github.com/gcc-mirror/gcc
+  PKG_SOURCE_VERSION:=$(PKG_REV)
+  PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_SOURCE_VERSION)
+  PKG_SOURCE:=$(PKG_NAME)-$(PKG_SOURCE_VERSION).tar.xz
   HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
 endif
 
@@ -129,7 +136,7 @@ GCC_CONFIGURE:= \
 		--disable-libmpx \
 		--disable-nls \
 		--with-tune=generic \
-		--enable-cld \
+		--enable-checking=release \
 		--with-system-zlib \
 		--with-default-libstdcxx-abi=gcc4-compatible \
 		--with-linker-hash-style=gnu \
