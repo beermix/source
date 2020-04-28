c763125339f6efd2ed446407c9d99b5de2e37b9a
AV <27rurk@gmail.com>
2020-04-28 14:10:29 -0500

    autocommit on change

--- target/linux/x86/image/geode.mk
+++ target/linux/x86/image/geode.mk
@@ -1,7 +1,6 @@
 define Device/generic
   DEVICE_TITLE := Generic x86/Geode
-  DEVICE_PACKAGES += kmod-crypto-cbc kmod-crypto-hw-geode kmod-ledtrig-gpio \
-	kmod-ledtrig-heartbeat kmod-ledtrig-netdev
+  DEVICE_PACKAGES += kmod-crypto-cbc
   GRUB2_VARIANT := legacy
 endef
 TARGET_DEVICES += generic
@@ -9,7 +8,6 @@ TARGET_DEVICES += generic
 define Device/geos
   $(call Device/generic)
   DEVICE_TITLE := Traverse Technologies Geos
-  DEVICE_PACKAGES += br2684ctl flashrom kmod-hwmon-lm90 kmod-mppe kmod-pppoa \
-	kmod-usb-ohci-pci linux-atm ppp-mod-pppoa pppdump pppstats soloscli tc
+  DEVICE_PACKAGES += kmod-usb-ohci-pci linux-atm
 endef
 TARGET_DEVICES += geos
