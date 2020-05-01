2978b55dca56d62afca5530b5306ef5be4d2aa6c
AV <27rurk@gmail.com>
2020-05-01 21:21:06 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -141,7 +141,6 @@ GCC_CONFIGURE:= \
 		--enable-checking=release \
 		--with-tune=generic \
 		--enable-cld \
-		--disable-libunwind-exceptions \
 		$(GRAPHITE_CONFIGURE) \
 		--with-host-libstdcxx=-lstdc++ \
 		$(SOFT_FLOAT_CONFIG_OPTION) \
