66786badb94911cf8055d211f5ddc2d4e692a251
AV <27rurk@gmail.com>
2020-05-02 21:14:38 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -226,8 +226,8 @@ GCC_MAKE:= \
 	export SHELL="$(BASH)"; \
 	$(MAKE) \
 		CFLAGS="$(HOST_CFLAGS)" \
-		CFLAGS_FOR_TARGET="$(TARGET_CFLAGS) -g1 -O3 -fstack-protector -Wl,-z -Wl,now -Wl,-z -Wl,relro -Wl,-z,max-page-size=0x1000" \
-		CXXFLAGS_FOR_TARGET="$(TARGET_CFLAGS) -g1 -O3 -Wl,-z,max-page-size=0x1000" \
+		CFLAGS_FOR_TARGET="$(TARGET_CFLAGS) -O3" \
+		CXXFLAGS_FOR_TARGET="$(TARGET_CFLAGS) -O3" \
 		GOCFLAGS_FOR_TARGET="$(TARGET_CFLAGS)"
 
 define Host/SetToolchainInfo
