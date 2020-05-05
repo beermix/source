6733949dc5f276683d30a1f80fb83dfcbd6fd5be
AV <27rurk@gmail.com>
2020-05-05 10:53:15 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -154,10 +154,12 @@ GCC_CONFIGURE:= \
 		--with-mpfr=$(TOPDIR)/staging_dir/host \
 		--with-mpc=$(TOPDIR)/staging_dir/host \
 		--disable-decimal-float \
+		--with-tune=generic \
 		--enable-gnu-indirect-function \
 		--disable-vtable-verify \
+		--enable-checking=release
 		--enable-clocale=gnu \
-		--with-tune=generic \
+		--with-linker-hash-style=gnu \
 		--with-diagnostics-color=always \
 		--enable-__cxa_atexit
 ifneq ($(CONFIG_mips)$(CONFIG_mipsel),)
