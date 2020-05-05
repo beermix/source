631a5e79eb7a3a30e26a3179943d03ce93bf594b
AV <27rurk@gmail.com>
2020-05-05 17:57:27 +0300

    autocommit on change

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -69,8 +69,8 @@ GLIBC_CONFIGURE:= \
 		--$(if $(CONFIG_SOFT_FLOAT),without,with)-fp \
 		--enable-kernel=4.19
 
-export libc_cv_forced_unwind=yes
-export libc_cv_c_cleanup=yes
+# export libc_cv_forced_unwind=yes
+# export libc_cv_c_cleanup=yes
 export ac_cv_header_cpuid_h=yes
 export HOST_CFLAGS := $(HOST_CFLAGS) -idirafter $(CURDIR)/$(PATH_PREFIX)/include
 
