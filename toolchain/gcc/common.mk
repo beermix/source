85f980574b397795e70fb97543b00283674c3d34
AV <27rurk@gmail.com>
2020-05-07 06:32:50 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -154,7 +154,6 @@ GCC_CONFIGURE:= \
 		--with-mpfr=$(TOPDIR)/staging_dir/host \
 		--with-mpc=$(TOPDIR)/staging_dir/host \
 		--disable-decimal-float \
-		--disable-vtable-verify \
 		--with-tune=generic \
 		--with-linker-hash-style=gnu \
 		--with-diagnostics-color=always \
