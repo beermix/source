d028ffe2f1225d91694aa2bd2d7e0e362024999e
AV <27rurk@gmail.com>
2020-05-11 05:52:40 +0300

    autocommit on change

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -66,13 +66,10 @@ GLIBC_CONFIGURE:= \
 		--without-cvs \
 		--enable-add-ons \
 		--enable-lock-elision \
-		--enable-stack-protector=yes \
 		--disable-debug \
 		--$(if $(CONFIG_SOFT_FLOAT),without,with)-fp \
 		--enable-kernel=5.4
 
-export libc_cv_forced_unwind=yes
-export libc_cv_c_cleanup=yes
 export libc_cv_ssp=no
 export libc_cv_ssp_strong=no
 export ac_cv_header_cpuid_h=yes
