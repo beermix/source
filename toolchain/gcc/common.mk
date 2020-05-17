165767f755c281037032c7f2e9eb03536063dfc2
AV <27rurk@gmail.com>
2020-05-18 01:31:09 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -44,14 +44,6 @@ ifeq ($(PKG_VERSION),9.3.0)
   PKG_HASH:=71e197867611f6054aa1119b13a0c0abac12834765fe2d81f35ac57f84f742d1
 endif
 
-ifeq ($(PKG_VERSION),8.4.1)
-  PKG_REV:=8-20200430
-  PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
-  PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
-  GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
-  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
-endif
-
 ifeq ($(PKG_VERSION),9.3.1)
   PKG_REV:=9.3.1-20200515
   PKG_SOURCE_URL:=https://dl.dropboxusercontent.com/s/h5zpklahnpus5e0
@@ -69,38 +61,6 @@ endif
 
 ifeq ($(PKG_VERSION),10.1.0)
   PKG_HASH:=b6898a23844b656f1b68691c5c012036c2e694ac4b53a8918d4712ad876e7ea2
-endif
-
-ifeq ($(PKG_VERSION),10.1.1)
-  PKG_VERSION:=10.1.1
-  PKG_REV:=10-20200516
-  PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
-  PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
-  GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
-  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
-#  PKG_REV:=bcf1d38
-#  PKG_SOURCE_PROTO:=git
-#  PKG_SOURCE_URL:=https://github.com/gcc-mirror/gcc
-#  PKG_SOURCE_VERSION:=$(PKG_REV)
-#  PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_SOURCE_VERSION)
-#  PKG_SOURCE:=$(PKG_NAME)-$(PKG_SOURCE_VERSION).tar.xz
-#  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
-endif
-
-ifeq ($(PKG_VERSION),11.0.0)
-  PKG_VERSION:=11.0.0
-  PKG_REV:=11-202005110
-  PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
-  PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
-  GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
-  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
-#  PKG_REV:=bcf1d38
-#  PKG_SOURCE_PROTO:=git
-#  PKG_SOURCE_URL:=https://github.com/gcc-mirror/gcc
-#  PKG_SOURCE_VERSION:=$(PKG_REV)
-#  PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_SOURCE_VERSION)
-#  PKG_SOURCE:=$(PKG_NAME)-$(PKG_SOURCE_VERSION).tar.xz
-#  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
 endif
 
 PATCH_DIR=../patches/$(GCC_VERSION)
