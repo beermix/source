c68a4c03c564d07caf6222371501117227e93db8
AV <27rurk@gmail.com>
2020-05-18 00:06:53 +0300

    autocommit on change

--- toolchain/uClibc/common.mk
+++ toolchain/uClibc/common.mk
@@ -7,7 +7,7 @@
 include $(TOPDIR)/rules.mk
 include $(INCLUDE_DIR)/target.mk
 
-PKG_VERSION:=1.0.32
+PKG_VERSION:=1.0.34
 
 PKG_NAME:=uClibc-ng
 PKG_SOURCE_URL = http://downloads.uclibc-ng.org/releases/$(PKG_VERSION)/
