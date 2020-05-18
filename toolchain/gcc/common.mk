dba7939d3d113bc9f97b9b6d0884e2f4b1ab622b
AV <27rurk@gmail.com>
2020-05-18 07:01:43 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -44,14 +44,6 @@ ifeq ($(PKG_VERSION),9.3.0)
   PKG_HASH:=71e197867611f6054aa1119b13a0c0abac12834765fe2d81f35ac57f84f742d1
 endif
 
-ifeq ($(PKG_VERSION),8.4.1)
-  PKG_REV:=8-20200430
-  PKG_SOURCE_URL:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(PKG_REV)
-  PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
-  GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
-  HOST_BUILD_DIR = $(BUILD_DIR_HOST)/gcc-$(PKG_REV)
-endif
-
 ifeq ($(PKG_VERSION),9.3.1)
   PKG_REV:=9.3.1-20200515
   PKG_SOURCE_URL:=https://dl.dropboxusercontent.com/s/h5zpklahnpus5e0
