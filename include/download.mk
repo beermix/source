5977aba1e41fdabfe57b23f5d477dd530e02fbd4
AV <27rurk@gmail.com>
2020-06-03 19:55:32 +0300

    autocommit on change

--- include/download.mk
+++ include/download.mk
@@ -51,6 +51,7 @@ dl_pack/bz2=bzip2 -c > $(1)
 dl_pack/gz=gzip -nc > $(1)
 dl_pack/xz=xz -T 7 -zc -7e > $(1)
 dl_pack/unknown=$(error ERROR: Unknown pack format for file $(1))
+dl_pack/unknown=$(error ERROR: Unknown pack format for file $(1))
 define dl_pack
 	$(if $(dl_pack/$(call ext,$(1))),$(dl_pack/$(call ext,$(1))),$(dl_pack/unknown))
 endef
