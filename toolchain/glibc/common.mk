8574a32d32416082b1e07ad95110c6ebb8957263
AV <27rurk@gmail.com>
2020-06-04 05:05:16 +0300

    autocommit on change

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -48,7 +48,7 @@ GLIBC_CONFIGURE:= \
 	BUILD_CC="$(HOSTCC)" \
 	$(TARGET_CONFIGURE_OPTS) \
 	CFLAGS="-O3 -m32 -march=bonnell -mstackrealign -g2  -Wl,-z,max-page-size=0x1000 $(filter-out -fomit-frame-pointer -fno-plt -march=bonnell -O2 -m32 -pipe -Os,$(call qstrip,$(TARGET_CFLAGS)))" \
-	export LDFLAGS="-Wl,-z,max-page-size=0x1000" \
+	LDFLAGS="-Wl,-z,max-page-size=0x1000" \
 	libc_cv_slibdir="/lib" \
 	use_ldconfig=no \
 	$(HOST_BUILD_DIR)/$(GLIBC_PATH)configure \
