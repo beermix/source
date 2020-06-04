c331070cc866079fa9d68e8fa71f333e08799f72
AV <27rurk@gmail.com>
2020-06-04 14:19:36 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -100,7 +100,7 @@ endif
 ifdef CONFIG_GCC_USE_GRAPHITE
   GRAPHITE_CONFIGURE:= --with-isl=$(TOPDIR)/staging_dir/host
 else
-  GRAPHITE_CONFIGURE:= --without-isl --without-cloog
+  GRAPHITE_CONFIGURE:= --enable-graphite=no --with-isl=no
 endif
 
 GCC_CONFIGURE:= \
