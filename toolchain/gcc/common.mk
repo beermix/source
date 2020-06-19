b0b41b9d2a7e77658f9dfa40a563cc3fab576e87
beermix <27rurk@gmail.com>
2020-06-19 15:13:17 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -136,8 +136,6 @@ GCC_CONFIGURE:= \
 		--with-mpc=$(TOPDIR)/staging_dir/host \
 		--disable-decimal-float \
 		--with-linker-hash-style=gnu \
-		--enable-checking=release \
-		--with-tune=generic \
 		--with-diagnostics-color=always \
 		--enable-__cxa_atexit
 ifneq ($(CONFIG_mips)$(CONFIG_mipsel),)
