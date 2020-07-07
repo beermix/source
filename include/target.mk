60d9b67d7fdc443e13ae858196b904f7587ec499
beermix <27rurk@gmail.com>
2020-07-07 15:22:57 +0300

    autocommit on change

--- include/target.mk
+++ include/target.mk
@@ -19,7 +19,7 @@ DEFAULT_PACKAGES.basic:=
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
-DEFAULT_PACKAGES.router:=dnsmasq iptables firewall
+DEFAULT_PACKAGES.router:=dnsmasq iptables ip6tables ppp ppp-mod-pppoe firewall odhcpd-ipv6only odhcp6c kmod-ipt-offload
 
 ifneq ($(DUMP),)
   all: dumpinfo
