629e93a19196db1ec2d2707fb0d0836540a3c7f9
beermix <27rurk@gmail.com>
2020-07-13 02:27:03 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -72,7 +72,7 @@ ifeq ($(PKG_VERSION),10.1.0)
 endif
 
 ifeq ($(PKG_VERSION),10.1.1)
-  PKG_REV:=10-20200704
+  PKG_REV:=10-20200711
   PKG_SOURCE_URL:=https://gcc.gnu.org/pub/gcc/snapshots/LATEST-10
   PKG_SOURCE:=gcc-$(PKG_REV).tar.xz
   GCC_DIR:=$(PKG_NAME)-$(GCC_VERSION)
