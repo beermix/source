cb6522c722f2635235e480a9d57c43c5369abedc
beermix <27rurk@gmail.com>
2020-08-29 02:39:02 +0300

    autocommit on change

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -1,7 +1,6 @@
 define Device/generic
   DEVICE_TITLE := Generic x86/64
-  DEVICE_PACKAGES += kmod-bnx2 kmod-e1000 kmod-forcedeth \
-	kmod-r8169
+  DEVICE_PACKAGES +=  kmod-forcedeth kmod-r8169
   GRUB2_VARIANT := generic
 endef
 TARGET_DEVICES += generic
--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -130,7 +130,6 @@ GCC_CONFIGURE:= \
 		--disable-decimal-float \
 		--with-diagnostics-color=always \
 		--with-linker-hash-style=gnu \
-		--disable-fixed-point \
 		--enable-__cxa_atexit
 ifneq ($(CONFIG_mips)$(CONFIG_mipsel),)
   GCC_CONFIGURE += --with-mips-plt
