78ced81061e39f8c38cfa59dcd304e5bc88ff84e
user <user@example.com>
2020-10-07 05:27:19 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -172,7 +172,7 @@ ifeq ($(CONFIG_TARGET_x86)$(CONFIG_USE_GLIBC)$(CONFIG_INSTALL_GCCGO),yyy)
   TARGET_CFLAGS+=-fno-split-stack
 endif
 
-TARGET_CFLAGS := -O2 $(filter-out -O%,$(TARGET_CFLAGS))
+#TARGET_CFLAGS := -O2 $(filter-out -O%,$(TARGET_CFLAGS))
 #TARGET_CFLAGS := $(filter-out -D_FORTIFY_SOURCE=%,$(TARGET_CFLAGS))
 
 GCC_MAKE:= \
