82d13d5c43b707f0e1ddc9b427c60df25a598328
Your Name <you@example.com>
2020-12-04 08:30:06 +0300

    autocommit on change

--- busspd2k/RunPerf.bat
+++
@@ -1,8 +0,0 @@
-REM  The following runs the program, records the results in log file
-REM  BusRead.txt and exits automatically. Running time is 100 msecs
-REM  per test and maximum memory tested default of 25% to 50% of RAM size.
-REM  Other parameters are KBytes kkkk for maximum memory size used,
-REM  Offset oo with oo between 1 and 31 for reading byte offset from
-REM  word boundary, Log nnn for alternative log file nnn.
-
-Start BusSpd2k Auto, MSeconds 100
--- busspd2k/SourceCode/RCtoObj.bat
+++
@@ -1,2 +0,0 @@
-rc busRes.rc
-cvtres /MACHINE:IX86 busRes.res 
