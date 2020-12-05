e197321bd567b526ca2b9df8162a09fb4d4e38f2
Your Name <you@example.com>
2020-12-05 09:13:03 +0300

    autocommit on change

--- package/aMy/rust/rustc-triple.mk
+++ package/aMy/rust/rustc-triple.mk
@@ -2,7 +2,7 @@
 # include $(TOPDIR)/feeds/packages/lang/rustup/rustc-triple.mk
 # below include $(TOPDIR)/rules.mk
 #
-# You will need to add --host=$(RUSTC_TARGET_ARCH) to your packages
+# You will need to add --host=i686-unknown-linux-musl to your packages
 # CONFIGURE_VARS to cross-compile for the target architecture
 
 ifeq ($(CONFIG_arm_v7),y)
@@ -99,7 +99,7 @@ RUSTC_TARGET_ARCH:=$(strip $(filter-out %sf, $(RUSTC_TARGET_ARCH_BASE)))
 endif
 
 # For Testing - Override
-RUSTC_TARGET_ARCH:=i686-unknown-linux-musl
+#RUSTC_TARGET_ARCH:=i686-unknown-linux-musl
 
 # More than one triple-target remains.
 ifneq ($(word 2, $(RUSTC_TARGET_ARCH)),)
