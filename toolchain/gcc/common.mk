12c47c6bdbdad419073418faa0c7eb8f4f204fe0
Your Name <you@example.com>
2020-12-11 12:43:18 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -129,6 +129,9 @@ GCC_CONFIGURE:= \
 		--disable-libstdcxx-pch \
 		--disable-fixed-point \
 		--with-linker-hash-style=gnu \
+		--with-arch=bonnell \
+		--with-tune=generic \
+		--enable-cld \
 		--disable-libgomp \
 		--disable-libmudflap \
 		--disable-multilib \
@@ -145,7 +148,6 @@ GCC_CONFIGURE:= \
 		--with-gmp=$(TOPDIR)/staging_dir/host \
 		--with-mpfr=$(TOPDIR)/staging_dir/host \
 		--with-mpc=$(TOPDIR)/staging_dir/host \
-		--disable-decimal-float \
 		--with-diagnostics-color=always \
 		--enable-__cxa_atexit
 ifneq ($(CONFIG_mips)$(CONFIG_mipsel),)
