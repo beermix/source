d1bee2b490c2e978b3fbab98f4e88f80f94bcfa4
Your Name <you@example.com>
2020-12-24 02:00:05 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -140,6 +140,10 @@ GCC_CONFIGURE:= \
 		--with-mpc=$(TOPDIR)/staging_dir/host \
 		--disable-decimal-float \
 		--with-linker-hash-style=gnu \
+		--disable-libstdcxx-pch \
+		--with-arch=bonnell \
+		--with-tune=generic \
+		--enable-cld \
 		--with-diagnostics-color=always \
 		--enable-__cxa_atexit
 ifneq ($(CONFIG_mips)$(CONFIG_mipsel),)
