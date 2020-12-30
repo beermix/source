74c3c214e9e826646552b52e8060d594d9720df5
Your Name <you@example.com>
2020-12-30 13:11:41 +0300

    autocommit on change

--- target/linux/x86/image/generic.mk
+++ target/linux/x86/image/generic.mk
@@ -1,6 +1,6 @@
 define Device/generic
   DEVICE_TITLE := Generic x86
-  DEVICE_PACKAGES += kmod-pcnet32 kmod-forcedeth
+  DEVICE_PACKAGES += kmod-r8169
   GRUB2_VARIANT := generic
 endef
 TARGET_DEVICES += generic
