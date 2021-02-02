c3d8056d23cc5321394ca0d920813419391a55c9
Your Name <you@example.com>
2021-02-02 13:47:18 +0300

    autocommit on change

--- package/base-files/files/lib/functions.sh
+++ package/base-files/files/lib/functions.sh
@@ -91,7 +91,7 @@ list() {
 	local len
 
 	config_get len "$CONFIG_SECTION" "${varname}_LENGTH" 0
-	[ "$len" = 0 ] && append CONFIG_LIST_STATE "${CONFIG_SECTION}_${varname}"
+	[ $len = 0 ] && append CONFIG_LIST_STATE "${CONFIG_SECTION}_${varname}"
 	len=$((len + 1))
 	config_set "$CONFIG_SECTION" "${varname}_ITEM$len" "$value"
 	config_set "$CONFIG_SECTION" "${varname}_LENGTH" "$len"
@@ -170,7 +170,7 @@ config_list_foreach() {
 
 	config_get len "${section}" "${option}_LENGTH"
 	[ -z "$len" ] && return 0
-	while [ "$c" -le "$len" ]; do
+	while [ $c -le "$len" ]; do
 		config_get val "${section}" "${option}_ITEM$c"
 		eval "$function \"\$val\" \"\$@\""
 		c="$((c + 1))"
@@ -188,8 +188,7 @@ default_prerm() {
 	fi
 
 	local shell="$(command -v bash)"
-	grep -s "^/etc/init.d/" "$root/usr/lib/opkg/info/${pkgname}.list" | while IFS= read -r i
-	do
+	for i in $(grep -s "^/etc/init.d/" "$root/usr/lib/opkg/info/${pkgname}.list"); do
 		if [ -n "$root" ]; then
 			${shell:-/bin/sh} "$root/etc/rc.common" "$root$i" disable
 		else
@@ -267,8 +266,7 @@ default_postinst() {
 
 		if grep -m1 -q -s "^/etc/uci-defaults/" "$filelist"; then
 			[ -d /tmp/.uci ] || mkdir -p /tmp/.uci
-			grep -s "^/etc/uci-defaults/" "$filelist" | while IFS= read -r i
-			do
+			for i in $(grep -s "^/etc/uci-defaults/" "$filelist"); do
 				( [ -f "$i" ] && cd "$(dirname $i)" && . "$i" ) && rm -f "$i"
 			done
 			uci commit
@@ -278,8 +276,7 @@ default_postinst() {
 	fi
 
 	local shell="$(command -v bash)"
-	grep -s "^/etc/init.d/" "$root$filelist" | while IFS= read -r i
-	do
+	for i in $(grep -s "^/etc/init.d/" "$root$filelist"); do
 		if [ -n "$root" ]; then
 			${shell:-/bin/sh} "$root/etc/rc.common" "$root$i" enable
 		else
@@ -296,9 +293,8 @@ default_postinst() {
 include() {
 	local file
 
-	for file in "$1"/*.sh; do
-		[ -e "$file" ] || break
-		. "$file"
+	for file in $(ls $1/*.sh 2>/dev/null); do
+		. $file
 	done
 }
 
